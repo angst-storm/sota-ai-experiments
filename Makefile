fd601f3c5056cff334da3f234930ac3cda71beb8
Kiprin Sergei <sergei@jinteki.dev>
2024-10-03 20:14:42 +0500

    added operators

--- Makefile
+++ Makefile
@@ -3,3 +3,9 @@ kind-cluster:
 
 destroy-kind-cluster:
 	kind delete cluster --name sota
+
+build:
+	docker build . -f deploy/airflow/Dockerfile -t customairflow:7
+
+load:
+	kind load docker-image customairflow:7 --name sota
--- airflow/dags/sample-dag.py
+++ airflow/dags/sample-dag.py
@@ -2,15 +2,32 @@ from airflow.decorators import dag
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
 from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
 from airflow.template.templater import LiteralValue
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.operators.email import EmailOperator
+from airflow.utils.trigger_rule import TriggerRule
 
 @dag()
-def spark_operator():
-    submit = SparkKubernetesOperator(
-        task_id="submit",
+def sample_dag():
+    postgres = PostgresOperator(
+        task_id='postgres',
+        postgres_conn_id='pg',
+        sql='SELECT 1'
+    )
+
+    spark_pi = SparkKubernetesOperator(
+        task_id="spark_pi",
         namespace="airflow",
         application_file=LiteralValue("dags/sparkapp.yaml")
     )
 
-    submit
+    email = EmailOperator(
+        task_id='send_email',
+        to='{{ var.value.admin_email }}',
+        subject='Внимание!',
+        html_content='<p>Все отработало хорошо</p>',
+        trigger_rule=TriggerRule.ALL_SUCCESS
+    )
 
-spark_operator()
+    postgres >> spark_pi >> email
+
+sample_dag()
