811576a939aabc31a303bd5d6cd14efb9f1cb939
Kiprin Sergei <sergei@jinteki.dev>
2024-10-03 16:24:14 +0500

    fixed dag

--- airflow/dags/sample-dag.py
+++ airflow/dags/sample-dag.py
@@ -8,29 +8,29 @@ from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOpera
 
 default_args={
    'depends_on_past': False,
-   'email': ['abcd@gmail.com'],
+   'email': ['serezha.kiprin2002@gmail.com'],
    'email_on_failure': False,
    'email_on_retry': False,
    'retries': 1,
    'retry_delay': timedelta(minutes=5)
 }
 with DAG(
-   'my-second-dag',
+   'sample-dag',
    default_args=default_args,
    description='simple dag',
    schedule_interval=timedelta(days=1),
    start_date=datetime(2022, 11, 17),
    catchup=False,
-   tags=['example']
+   tags=['test']
 ) as dag:
    t1 = SparkKubernetesOperator(
-       task_id='n-spark-pi',
+       task_id='spark-pi',
        trigger_rule="all_success",
        depends_on_past=False,
        retries=3,
-       application_file="new-spark-pi.yaml",
-       namespace="spark-jobs",
-       kubernetes_conn_id="myk8s",
+       application_file="sparkapp.yaml",
+       namespace="spark-operator",
+       kubernetes_conn_id="kind-cluster",
        api_group="sparkoperator.k8s.io",
        api_version="v1beta2",
        do_xcom_push=True,
