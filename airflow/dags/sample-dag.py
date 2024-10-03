c7ec4c3d0e4ed5365664a14ecc5c56b0aea486fd
Kiprin Sergei <sergei@jinteki.dev>
2024-10-03 16:35:07 +0500

    fixed dag

--- airflow/dags/sample-dag.py
+++ airflow/dags/sample-dag.py
@@ -2,37 +2,27 @@ from airflow import DAG
 from datetime import timedelta, datetime
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
 from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
-from airflow.models import Variable
-from kubernetes.client import models as k8s
-from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 
-default_args={
-   'depends_on_past': False,
-   'email': ['serezha.kiprin2002@gmail.com'],
-   'email_on_failure': False,
-   'email_on_retry': False,
-   'retries': 1,
-   'retry_delay': timedelta(minutes=5)
-}
-with DAG(
-   'sample-dag',
-   default_args=default_args,
-   description='simple dag',
-   schedule_interval=timedelta(days=1),
-   start_date=datetime(2022, 11, 17),
-   catchup=False,
-   tags=['test']
-) as dag:
-   t1 = SparkKubernetesOperator(
-       task_id='spark-pi',
-       trigger_rule="all_success",
-       depends_on_past=False,
-       retries=3,
-       application_file="sparkapp.yaml",
-       namespace="spark-operator",
-       kubernetes_conn_id="kind-cluster",
-       api_group="sparkoperator.k8s.io",
-       api_version="v1beta2",
-       do_xcom_push=True,
-       dag=dag
+@DAG(
+   schedule=None,
+   start_date= datetime(2024, 3, 1),
+)
+def spark_operator():
+   submit = SparkKubernetesOperator(
+        task_id="spark-pi",
+        namespace="spark-operator",
+        application_file="sparkapp.yaml",
+        do_xcom_push=True,
+        params={"app_name": "spark-pi"},
+    )
+
+   submit_sensor = SparkKubernetesSensor(
+        task_id="submit_sensor",
+        namespace="spark-operator",
+        application_name="{{ task_instance.xcom_pull(task_ids='submit')['metadata']['name'] }}",
+        attach_log=True,
    )
+
+   submit >> submit_sensor
+
+spark_operator()
