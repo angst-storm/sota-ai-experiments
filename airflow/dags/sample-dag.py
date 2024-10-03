085d332a95e1ed1a9453baa33bb39be5d19b182c
Kiprin Sergei <sergei@jinteki.dev>
2024-10-03 17:07:36 +0500

    fixed dag

--- airflow/dags/sample-dag.py
+++ airflow/dags/sample-dag.py
@@ -1,13 +1,24 @@
 from airflow.decorators import dag
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
+from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
 
 @dag()
 def spark_operator():
-   SparkKubernetesOperator(
-        task_id="spark-pi",
-        namespace="spark-operator",
+    submit = SparkKubernetesOperator(
+        task_id="submit",
+        namespace="airflow",
         application_file="sparkapp.yaml",
-        do_xcom_push=True
+        do_xcom_push=True,
+        params={"app_name": "spark-pi"},
     )
+
+    submit_sensor = SparkKubernetesSensor(
+        task_id="submit_sensor",
+        namespace="airflow",
+        application_name="{{ task_instance.xcom_pull(task_ids='submit')['metadata']['name'] }}",
+        attach_log=True,
+    )
+
+    submit >> submit_sensor
 
 spark_operator()
